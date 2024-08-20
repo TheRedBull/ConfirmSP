5b804b83b13cd2529aad485bb880c51f19b44031
Axel Clifford <axelclifford@gmail.com>
2024-08-19 20:01:57 -0600

    Fixes 11.0.2 API change

--- csp-private.lua
+++ csp-private.lua
@@ -2,11 +2,11 @@
 local _
 local assert = _G.assert
 local ADDON, P = ...
-local GetAddOnMetadata = _G.GetAddOnMetadata; assert(GetAddOnMetadata ~= nil,'GetAddOnMetadata')
+local GetAddOnMetadata = _G.C_AddOns.GetAddOnMetadata; assert(C_AddOns.GetAddOnMetadata ~= nil,'GetAddOnMetadata')
 local LibStub = _G.LibStub; assert(LibStub ~= nil,'LibStub')
 P.L = LibStub("AceLocale-3.0"):GetLocale(ADDON, false) -- don't print error if no locale
 P.TITLE = P.L["TITLE"]
-P.VERSION = (GetAddOnMetadata(ADDON, "Version") or "") .. " " .. (GetAddOnMetadata(ADDON, "X-Date") or "")
+P.VERSION = (C_AddOns.GetAddOnMetadata(ADDON, "Version") or "") .. " " .. (C_AddOns.GetAddOnMetadata(ADDON, "X-Date") or "")
 
 P.TIMER = 0.5 -- check pop-ups twice every second
 --P.TIMER = 2.0 -- check pop-ups every two seconds
